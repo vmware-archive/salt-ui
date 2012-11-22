a75cea0a5eec81da7be2f5775f2efcb0c1439561
Thomas S Hatch <thatch45@gmail.com>
2012-11-21 22:34:00 -0700

    Add Master Config functions

--- js/controllers.js
+++ js/controllers.js
@@ -16,3 +16,22 @@ function MinionDetailCtrl($scope, $routeParams) {
 }
 
 // MinionDetailCtrl.$inject = ['$scope', '$routeParams'];
+
+
+// Return the master configuration values
+function MasterConfigValues($scope, $http) {
+    $http.post('/', {
+        client: 'wheel',
+        fun: 'config.values'}).success(function(data) {
+            $scope.master_config = data;
+        });
+}
+
+// Apply a configuration value to the master config
+function MasterConfigApply($scope, $http, key, value) {
+    $http.post('/', {
+        client: 'wheel',
+        key: key,
+        value: value
+    });
+}
