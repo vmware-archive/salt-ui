d9d6b1a38a4c3c86499771af19f26467d6c2c45d
Seth House <seth@eseth.com>
2012-12-10 18:25:20 -0700

    Don't declare saltui.models module more than once

--- js/models.js
+++ js/models.js
@@ -1,9 +1,10 @@
+var saltui_models = angular.module('saltui.models', ['ngResource']);
+
 /**
  * A resource object to fetch data on minion(s)
  *
  */
-angular.module('saltui.models', ['ngResource'])
-.factory('Minion', ['$resource', function($resource){
+saltui_models.factory('Minion', ['$resource', function($resource){
     var Minion = $resource('/minions/:mid', {mid: '@mid'}, {
         all: {method: 'GET'}
     });
@@ -15,12 +16,10 @@ angular.module('saltui.models', ['ngResource'])
  * A resource object to fetch data on job(s)
  *
  */
-angular.module('saltui.models', ['ngResource'])
-.factory('Jobs', ['$resource', function($resource){
+saltui_models.factory('Jobs', ['$resource', function($resource){
     var Jobs = $resource('/jobs/:jid', {jid: '@jid'}, {
         all: {method: 'GET'}
     });
 
     return Jobs;
 }]);
-
