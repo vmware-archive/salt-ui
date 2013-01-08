97947d4f56265e4bc726be503818f38ae47ef944
Seth House <seth@eseth.com>
2013-01-08 16:35:17 -0700

    Don't need comma-first notation

--- js/saltui.js
+++ js/saltui.js
@@ -1,42 +1,43 @@
 /**
  * RequireJS configuration
  */
-requirejs.config(
+requirejs.config({
     // By default load any module IDs from js/lib
-    { baseUrl: 'js/lib'
+    baseUrl: 'js/lib',
     // except if the module ID starts with...
-    , paths:
-        { binders: '../binders'
-        , conf: '../conf'
-        , elements: '../elements'
-        , formatters: '../formatters'
-        , models: '../models'
-        , utils: '../utils'
-        , tmpl: '../../tmpl'
-        }
+    paths: {
+        binders: '../binders',
+        conf: '../conf',
+        elements: '../elements',
+        formatters: '../formatters',
+        mixins: '../mixins',
+        models: '../models',
+        utils: '../utils',
+        tmpl: '../../tmpl',
+    },
     // shim non-AMD libs
-    , shim:
-        { 'underscore': {exports: '_'}
-        , 'rivets': {exports: 'rivets'}
-        }
+    shim: {
+        'underscore': {exports: '_'},
+        'rivets': {exports: 'rivets'},
+    },
     // DEBUG: bust caches
-    , urlArgs: 'bust=' +  (new Date()).getTime()
-    }
-);
+    urlArgs: 'bust=' +  (new Date()).getTime(),
+});
 
 /**
 Main salt-ui entry-point
 
 @module saltui
 **/
-requirejs(
-    [ 'domReady!'
-    , 'elements/main'
-    , 'formatters/main'
-    , 'binders/main'
-    , 'conf/main'
-    ],
-    function(document) {
+requirejs([
+    'domReady!',
+    'binders/init',
+    'conf/init',
+    'elements/init',
+    'formatters/init',
+    'mixins/init',
+    'models/init',
+    ], function(document) {
         'use strict';
         return null;
     }
