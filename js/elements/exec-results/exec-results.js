a1285c9a1d3a77aa8a8c22cabce971580edc0feb
Seth House <seth@eseth.com>
2013-01-18 22:49:59 -0700

    Added displaying results from an execution

--- js/elements/exec-results/exec-results.js
+++ js/elements/exec-results/exec-results.js
@@ -4,11 +4,33 @@ A custom element to execute Salt modules via salt-api
 @module saltui.elements
 @submmodule exec-results
 **/
-define(['text!./template.html'], function(template) {
+define(function(require) {
     'use strict';
 
+    var xhr = require('utils/xhr'),
+        drawtree = require('./tree');
+
     var exec_results = {
-        content: template,
+        onCreate: function() {
+            document.addEventListener('exec', this.get_results);
+        },
+        methods: {
+            get_results: function(e) {
+                return xhr('GET', '/jobs/' + e.jid).get('return').get(0)
+                .then(this.render);
+            },
+            render: function(result) {
+                // this.innerHTML = JSON.stringify(result);
+
+                // empty the node
+                while (this.firstChild) {
+                    this.removeChild(this.firstChild);
+                }
+
+                // fill the node with the output
+                drawtree(result, this);
+            },
+        },
     };
 
     return exec_results;
