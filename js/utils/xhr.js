7095f9a46999d6219ac8a91f2bab2655d5a42529
Seth House <seth@eseth.com>
2013-01-24 12:26:20 -0700

    Refactored xhr to take an object of params instead of individual params

--- js/utils/xhr.js
+++ js/utils/xhr.js
@@ -4,39 +4,50 @@ An XMLHttpRequest wrapper that returns promises
 @module saltui.utils
 @submodule xhr
 
-@param {String} method The HTTP method to use for the request (GET, POST, etc)
-@param {String} path The URL for the request
-@param {Object} [data] Any data to put in the request; GET requests will use
-    URL parameters, POST requests will send JSON as the reqeust body
-@param {Object} [headers] Any additional headers to put in the request
+@param {Object} opts An object of xhr params
+    @param {String} opts.method The HTTP method to use for the request (GET,
+        POST, etc)
+    @param {String} opts.path The URL for the request
+    @param {Object} [opts.data] Any data to put in the request; GET requests
+        will use URL parameters, POST requests will send JSON as the reqeust
+        body
+    @param {Object} [opts.headers] Any additional headers to put in the request
 
 @returns {Promise} result A JSON representation of the HTTP response
 
 @example
-    var req = xhr('get', '/path');
+    var req = xhr({method: 'get', path: '/path'});
     req.then(function (result) {
         console.log(result);
     };
 **/
-define(['conf/routes', 'q', 'underscore'], function (routes, Q, _) {
+define(function (require) {
     'use strict';
 
-    function xhr(method, path, data, headers) {
+    var routes = require('conf/routes'),
+        Q = require('q');
+
+    function xhr(opts) {
         var deferred = Q.defer(),
             req = new XMLHttpRequest(),
-            default_headers = {
+            headers = {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'X-Requested-With': 'XMLHttpRequest',
             };
 
         // Begin building the request object
-        req.open(method.toUpperCase(), path, true);
+        req.open(opts.method.toUpperCase(), opts.path, true);
 
         // Add new request headers and/or override the default values
-        _.each(_.defaults(headers || {}, default_headers),
-                function(value, key) {
-            req.setRequestHeader(key, value);
+        if (!opts.headers) opts.headers = {};
+        Object.keys(opts.headers).forEach(function(key) {
+            headers[key] = opts.headers[key];
+        });
+
+        // Add all headers to the request
+        Object.keys(headers).forEach(function(key) {
+            req.setRequestHeader(key, headers[key]);
         });
 
         // Resolve or reject the deferred object based on the response code
@@ -48,6 +59,7 @@ define(['conf/routes', 'q', 'underscore'], function (routes, Q, _) {
             if ((req.status >= 200 && req.status < 300) || req.status === 304) {
                 deferred.resolve(e.target.response);
             } else {
+                // Show the login form if server reponds with a 401
                 if (req.status === 401) {
                     window.location.hash = routes.get_url('login');
                 }
@@ -58,15 +70,11 @@ define(['conf/routes', 'q', 'underscore'], function (routes, Q, _) {
         };
 
         // Convert the request data to a string
-        req.send(JSON.stringify(data));
+        req.send(JSON.stringify(opts.data));
 
-        // Convert the response data to an object (if possible)
+        // Convert the response data to an object
         return deferred.promise.then(function(result) {
-            try {
-                return JSON.parse(result);
-            } catch (e) {
-                return result;
-            }
+            return JSON.parse(result);
         });
     }
 
