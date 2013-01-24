7488f8e7c97a22580099504e5082b8a6a37153fb
Seth House <seth@eseth.com>
2013-01-24 01:34:24 -0700

    Moved @torhve 's "Running..." spinner into x-exec-results template

--- js/elements/exec-results/exec-results.js
+++ js/elements/exec-results/exec-results.js
@@ -7,17 +7,28 @@ A custom element to execute Salt modules via salt-api
 define(function(require) {
     'use strict';
 
-    var xhr = require('utils/xhr'),
+    var template = require('text!./template.html'),
+        xhr = require('utils/xhr'),
+        rivets = require('rivets'),
         drawtree = require('./tree');
 
     var exec_results = {
         onCreate: function() {
             document.addEventListener('exec', this.get_results);
+            this.xtag.inprogress = false;
+            rivets.bind(this, {vm: this.xtag});
         },
         methods: {
             get_results: function(e) {
+                var that = this;
+                this.xtag.inprogress = true;
+                this.innerHTML = template;
+
                 return xhr('GET', '/jobs/' + e.jid).get('return').get(0)
-                .then(this.render);
+                .then(function() {
+                    that.xtag.inprogress = false;
+                })
+               .then(this.render);
             },
             render: function(result) {
                 // this.innerHTML = JSON.stringify(result);
