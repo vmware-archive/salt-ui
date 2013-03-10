8b25079c884e9a83906f382f3f594832bd62cd60
Seth House <seth@eseth.com>
2013-03-10 04:00:02 -0600

    Added a utils of functional helpers

---
+++ js/utils/func.js
@@ -0,0 +1,242 @@
+/**
+Functional-style helpers cobbled together from many-a-place
+
+var ƒ = require('utils/func');
+ƒ.merge(obj1, obj2});
+**/
+define(function() {
+    'use strict';
+
+    var __slice = Array.prototype.slice,
+        __push = Array.prototype.push;
+
+    /**
+    merge
+    **/
+    function merge(target, source) {
+        return Object.keys(source).reduce(function(accum, key) {
+            return (accum[key] = source[key]) && accum;
+        }, target);
+    }
+
+    /**
+    extend
+    **/
+    function extend() {
+        return __slice.call(arguments).reduce(function(target, source) {
+            return merge(target, source);
+        });
+    }
+
+    /**
+    An identity (as in mathmatics) function; returns what is passed to it
+
+    @param x
+    @return x
+    **/
+    function identity(x) { return x }
+
+    /**
+    Extract the arguments of a function as an array
+
+    From Prototype.js; MIT License
+    **/
+    function argumentNames(fn) {
+        var names = fn.toString().match(/^[\s\(]*function[^(]*\(([^)]*)\)/)[1]
+            .replace(/\/\/.*?[\r\n]|\/\*(?:.|[\r\n])*?\*\//g, '')
+            .replace(/\s+/g, '').split(',');
+        return names.length === 1 && !names[0] ? [] : names;
+    }
+
+    /**
+    From http://underscorejs.org; MIT License
+    **/
+    var compose = function() {
+        var funcs = arguments;
+        return function() {
+            var args = arguments;
+            for (var i = funcs.length - 1; i >= 0; i -= 1) {
+                args = [funcs[i].apply(this, args)];
+            }
+            return args[0];
+        };
+    };
+
+    /**
+    Wraps the first function inside of the wrapper function, passing it as the
+    first argument. This allows the wrapper to execute code before and after
+    the function runs, adjust the arguments, and execute it conditionally.
+
+    From http://underscorejs.org; MIT License
+    **/
+    function wrap(func, wrapper) {
+        return function() {
+            var args = [func];
+            __push.apply(args, arguments);
+            return wrapper.apply(this, args);
+        };
+    }
+
+    /**
+    From http://allong.es; MIT License
+    **/
+    function variadic (fn) {
+        var fnLength = fn.length;
+
+        if (fnLength < 1) {
+            return fn;
+        }
+        else if (fnLength === 1)  {
+            return function () {
+                return fn.call(this, __slice.call(arguments, 0));
+            };
+        }
+        else {
+            return function () {
+                var numberOfArgs = arguments.length,
+                    namedArgs = __slice.call(arguments, 0, fnLength - 1),
+                    numberOfMissingNamedArgs = Math.max(
+                        fnLength - numberOfArgs - 1, 0),
+                    argPadding = new Array(numberOfMissingNamedArgs),
+                    variadicArgs = __slice.call(arguments, fn.length - 1);
+
+                return fn.apply(this,
+                    namedArgs.concat(argPadding).concat([variadicArgs]));
+            };
+        }
+    }
+
+    /**
+    From http://allong.es; MIT License
+
+    Applies the first argument, returns a variadic function taking the rest
+    **/
+    function applyFirst (fn, first) {
+        return variadic( function (args) {
+            return fn.apply(this, [first].concat(args));
+        });
+    }
+
+    /**
+    From http://allong.es; MIT License
+
+    Applies one or more arguments in the leftmost positions
+    **/
+    var applyLeft = variadic( function (fn, args) {
+        return variadic( function (remainingArgs) {
+            return fn.apply(this, args.concat(remainingArgs));
+        });
+    });
+
+    /**
+    Send a message/invoke a method on the receiver.
+
+    From http://allong.es; MIT License
+    **/
+    var send = variadic( function (methodName, args) {
+        return variadic( function (receiver, remainingArgs) {
+            var fn = receiver[methodName];
+            return fn.apply(receiver, args.concat(remainingArgs));
+        });
+    });
+
+    /**
+    Like send() but with an explicit context
+    **/
+    function sendWithCtx(ctx, attr) {
+        var arg = __slice.call(arguments, 2);
+        return ctx[attr].apply(ctx, arg);
+    }
+
+    /**
+    transforms a polyadic function into a chain of unary
+    functions
+
+    From http://allong.es; MIT License
+
+    @example
+        curry(function (x, y) { return x })
+        //=> function (x) { return function (y) { return x } }
+    **/
+    function curry (fn) {
+        var arity = fn.length;
+
+        return given([]);
+
+        function given (argsSoFar) {
+            return function curried () {
+                var updatedArgsSoFar = argsSoFar.concat(
+                    __slice.call(arguments, 0));
+
+                if (updatedArgsSoFar.length >= arity) {
+                    return fn.apply(this, updatedArgsSoFar);
+                }
+                else return given(updatedArgsSoFar);
+            };
+        }
+    }
+
+    /**
+    From http://allong.es; MIT License
+    **/
+    function fluent(fn) {
+        return function() {
+            fn.apply(this, arguments);
+            return this;
+        };
+    }
+
+    /**
+    From http://allong.es; MIT License
+    **/
+    function maybe(fn) {
+        return function() {
+            if (arguments.length === 0) return;
+
+            for (var i; i < arguments.length; i += 1) {
+                if (arguments[i] === null) return;
+            }
+
+            return fn.apply(this, arguments);
+        };
+    }
+
+    /**
+    From http://allong.es; MIT License
+    **/
+    function memoized (fn, keymaker) {
+        var lookupTable = {};
+
+        /*jshint expr: true */
+        keymaker || (keymaker = function (args) {
+            return JSON.stringify(args);
+        });
+
+        return function () {
+            var key = keymaker.call(this, arguments);
+
+            return lookupTable[key] || (
+                lookupTable[key] = fn.apply(this, arguments)
+            );
+        };
+    }
+
+    return {
+        applyFirst: applyFirst,
+        applyLeft: applyLeft,
+        argumentNames: argumentNames,
+        compose: compose,
+        curry: curry,
+        extend: extend,
+        fluent: fluent,
+        identity: identity,
+        maybe: maybe,
+        memoized: memoized,
+        merge: merge,
+        send: send,
+        sendWithCtx: sendWithCtx,
+        variadic: variadic,
+        wrap: wrap,
+    };
+
+});
