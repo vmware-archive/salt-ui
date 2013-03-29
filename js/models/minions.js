83a5685ecfeeee95c4de844eefd5f1a2428a268a
Seth House <seth@eseth.com>
2013-03-28 21:41:33 -0600

    Refactored minions model to use withCachedSync() mixin

--- js/models/minions.js
+++ js/models/minions.js
@@ -7,63 +7,29 @@ Minions and the grains for each
 define(function(require) {
     'use strict';
 
-    var xhr = require('utils/xhr'),
-        Q = require('q'),
-        _ = require('underscore');
+    var withCachedSync = require('./mixins/withCachedSync');
 
-    var minions = {
-
-        // A cache of the last return from the API.
-        _result: {},
-        // An in-progress AJAX request
-        _promise: null,
+    function Model() {
+        this.lowstate = [{client: 'local', tgt: '*', fun: 'grains.items'}];
+        this.result = {};
 
         /**
         Update the result cache
         **/
-        _update: function(result) {
-            this._result = result;
-        },
-
-        /**
-        Fetch grains for all minions from the API
-        @returns {Promise}
-        **/
-        sync: function() {
-            // If we get a call while a call is already running, return the
-            // promise for the one already running
-            if (this._promise === null) {
-                this._promise = xhr('POST', '/',
-                    [{client: 'local', tgt: '*', fun: 'grains.items'}])
-                .get('return').get(0)
-                .then(this._update.bind(this))
-                .fin(function() {
-                    this._promise = null;
-                }.bind(this));
-            }
-
-            return this._promise;
-        },
+        this.update = function(result) {
+            this.result = result['return'][0];
+        };
 
         /**
         Return the cached minion by id
         @return {Object}
         **/
-        get_minion: function(id) {
-           return this._result[id];
-        },
+        this.get_minion = function(id) {
+           return this.result[id];
+        };
+    }
 
-        /**
-        Returns the cached copy of results or queries the API for new results
-        @returns {Promise}
-        **/
-        get_result: function() {
-            if (! _.isEmpty(this._result)) {
-                return Q.fcall(function(){ return this._result; }.bind(this));
-            }
-            return this.sync();
-        },
-    };
-
-    return minions;
+    var model = new Model();
+    withCachedSync.call(model);
+    return model;
 });
