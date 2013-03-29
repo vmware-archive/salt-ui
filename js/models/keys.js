69cf70a5621b4bc8e544228021a3fa03734d50c2
Seth House <seth@eseth.com>
2013-03-28 23:56:28 -0600

    Added model for grabbing minion keys

---
+++ js/models/keys.js
@@ -0,0 +1,42 @@
+/**
+Description
+**/
+define(function(require) {
+    'use strict';
+
+    var withCachedSync = require('./mixins/withCachedSync');
+
+    function Model() {
+        this.lowstate = [
+            {client: 'wheel', fun: 'key.finger', match: '*'},
+            {client: 'wheel', fun: 'key.list_all'},
+        ];
+
+        this.update = function(result) {
+            var ret = result['return'],
+                m = ret[0].minions;
+
+            return {
+                pending: minion_list(ret[1].minions_pre, m),
+                accepted: minion_list(ret[1].minions, m),
+                rejected: minion_list(ret[1].minions_rejected, m),
+            };
+        };
+    }
+
+    /**
+    Helper func for above
+
+    @return {Array} A list of objects containing the id and fingerprint for
+        each minion
+    **/
+    function minion_list(list, obj) {
+        return list.map(function(val) {
+            return {id: val, fp: obj[val]};
+        });
+    }
+
+    var model = new Model();
+    withCachedSync.call(model);
+    return model;
+});
