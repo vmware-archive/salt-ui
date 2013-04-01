f5ca2d90713aab9b526be6b2e98218efbf1dff8b
Seth House <seth@eseth.com>
2013-04-01 09:36:12 -0600

    Added keys to models init

--- js/models/init.js
+++ js/models/init.js
@@ -5,6 +5,7 @@ define(function(require) {
     'use strict';
 
     var models = {
+        keys:           require('models/keys'),
         minions:        require('models/minions'),
         sysdoc:         require('models/sysdoc'),
     };
