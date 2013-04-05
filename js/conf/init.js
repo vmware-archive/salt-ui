c6ff3fc6da10306399bfc3cf07a1c10e0a7995a6
Seth House <seth@eseth.com>
2013-04-04 22:05:57 -0600

    Load shims earlier
    All the init files get loaded at the same time, then the files those
    init files reference are loaded. Meaning the shims files were not
    getting loaded early enough.

--- js/conf/init.js
+++ js/conf/init.js
@@ -7,7 +7,6 @@ define(function(require) {
     'use strict';
 
     var configs = {
-        shims:          require('conf/shims/init'),
         routes:         require('conf/routes'),
         rivets:         require('conf/rivets'),
     };
