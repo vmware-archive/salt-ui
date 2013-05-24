bdb43bb18cf19b02750c13b324f46c8a1e23881b
Seth House <seth@eseth.com>
2013-05-24 03:25:45 -0600

    Add query string flag to stub the xhr() backend using fixtures

--- js/saltui.js
+++ js/saltui.js
@@ -1,33 +1,47 @@
 /**
  * RequireJS configuration
  */
-requirejs.config({
-    // By default load any module IDs from js/lib
-    baseUrl: 'js/lib',
-    // except if the module ID starts with...
-    paths: {
-        binders: '../binders',
-        conf: '../conf',
-        crystals: '../crystals',
-        elements: '../elements',
-        formatters: '../formatters',
-        models: '../models',
-        shims: '../shims',
-        transforms: '../transforms',
-        utils: '../utils',
-        viewmodels: '../viewmodels',
+;(function() {
+    var config = {
+        // By default load any module IDs from js/lib
+        baseUrl: 'js/lib',
+        // except if the module ID starts with...
+        paths: {
+            binders: '../binders',
+            conf: '../conf',
+            crystals: '../crystals',
+            elements: '../elements',
+            formatters: '../formatters',
+            models: '../models',
+            shims: '../shims',
+            transforms: '../transforms',
+            utils: '../utils',
+            viewmodels: '../viewmodels',
 
-        fixtures: '../../fixtures',
-        tmpl: '../../tmpl',
-    },
-    // shim non-AMD libs
-    shim: {
-        'd3': {exports: 'd3'},
-        'path': {exports: 'Path'},
-    },
-    // DEBUG: bust caches
-    urlArgs: 'bust=' +  (new Date()).getTime(),
-});
+            fixtures: '../../fixtures',
+            tmpl: '../../tmpl',
+        },
+        // shim non-AMD libs
+        shim: {
+            'd3': {exports: 'd3'},
+            'path': {exports: 'Path'},
+        },
+        // DEBUG: bust caches
+        urlArgs: 'bust=' +  (new Date()).getTime(),
+    };
+
+    // If demo=true is in the query string swap out the xhr backend for a
+    // local-only fixtures-based stub
+    if (window.location.search.indexOf('demo=true') !== -1) {
+        config.map = {
+            '*': {
+                'utils/xhr': 'utils/fixtures',
+            },
+        };
+    }
+
+    requirejs.config(config);
+})();
 
 /**
 Main salt-ui entry-point
