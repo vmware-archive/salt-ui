32eb35b88751fe4d5980dc5880442309a1ba88db
Seth House <seth@eseth.com>
2013-05-29 11:24:05 -0600

    Added viewmodel for the master config

--- js/viewmodels/init.js
+++ js/viewmodels/init.js
@@ -5,6 +5,7 @@ define(function(require) {
     'use strict';
 
     var vms = {
+        config:     require('./config'),
         exec:       require('./exec'),
         login:      require('./login'),
         minions:    require('./minions'),
